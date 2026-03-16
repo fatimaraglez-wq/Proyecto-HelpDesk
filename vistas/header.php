218f75c80b96e70945849c586ed9754a62f8fd08
Fatis27 <fatima.raglez@gmail.com>
2026-03-16 00:40:25 -0600

    En esta sección agregamos el logo y las vistas :)

---
+++ vistas/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="../public/css/plantilla.css">
+    <title>Help-Desk</title>
+</head>
+<body>
+    <!-- Navigation -->
+     <nav class="navbar navbar-expand-lg navbar-light bg-light static-top mb-5 shadow">
+        <div class="container">
+            <a class="navbar-brand" href="inicio.php">Help - Desk</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="inicio.php">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="misDispositivos.php">Mis dispositivos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="misReportes.php">Reportes Soporte</a>
+                    </li>
+ 
+                    <!--VISTAS DEL ADMINISTRADOR-->
+                    <li class="nav-item">
+                        <a class="nav-link" href="git add .">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="asignacion.php">Asignacion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="reportes.php">Reportes</a>
+                    </li>
+
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown"
+                         role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Usuario
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Editar Datos</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="../procesos/usuarios/login/salir.php">Salir</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
